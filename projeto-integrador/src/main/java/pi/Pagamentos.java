3f6a381e411165ff9db6e6af9e95633560015d6c
Gustavo-Unifio <272164@unifio.edu.br>
2025-09-17 23:40:31 +0000

    Aula de teste e criação das tabelas pro pi

---
+++ projeto-integrador/src/main/java/pi/Pagamentos.java
@@ -0,0 +1,23 @@
+package pi;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Pagamentos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    public Double valor;
+    public String metodoPagamento;
+    public String Status;
+    public LocalDate dataPagamento;
+    public String comprovanteUrl;
+    @ManyToOne
+    public Contratacoes contratacoes; 
+}
---
+++ projeto-integrador/src/main/java/pi/Servicos.java
@@ -0,0 +1,16 @@
+package pi;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Servicos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    public Integer id;
+    public String nome;
+    public String descricao;
+    public String categoria;
+}
