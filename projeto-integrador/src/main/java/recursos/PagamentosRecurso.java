2fd6bb5daa8fa203ed6476fb6863c5f6e3478cf4
Gustavo-Unifio <272164@unifio.edu.br>
2025-11-14 01:58:17 +0000

    Colocado o DELETE no PagamentoRecurso

--- projeto-integrador/src/main/java/recursos/PagamentosRecurso.java
+++ projeto-integrador/src/main/java/recursos/PagamentosRecurso.java
@@ -5,9 +5,11 @@ import java.util.List;
 import entidades.Pagamentos;
 import io.quarkus.panache.common.Sort;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path ("pagamentos")
 public class PagamentosRecurso {
@@ -20,5 +22,16 @@ public class PagamentosRecurso {
     @Transactional
     public void salvar (Pagamentos pagamentos) {
         pagamentos.persist();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void excluir (@PathParam("id") Integer id) {
+        Pagamentos pagamentos = Pagamentos.findById(id);
+
+        if (pagamentos != null) {
+            pagamentos.delete();
+        }
     }
 }
